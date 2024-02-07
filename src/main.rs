db27e77547873b9b2a01e78ce08080aae7a9eca0
Niek van de Pas <niek_pas@live.nl>
2024-02-07 14:14:04 +0100

    bak

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
+use crate::reference::CsvError::{ParseError, ReadError};
+use std::path::PathBuf;
+
 use tabled::Table;
+extern crate shellexpand;
 
 mod reference;
 
 fn main() {
-    let refs = reference::example_references();
+    println!();
+    println!();
+    let home = std::env::var("HOME").unwrap();
+    let path_str = format!("{}{}", home, "/.citeseer/references.csv");
+    let references_path = PathBuf::from(path_str);
+    let refs = reference::references_from_csv(&references_path);
 
-    let table = Table::new(refs).to_string();
-    println!("{}", table);
+    match refs {
+        Ok(references) => {
+            // for reference in references.iter() {
+            //     println!("found result: {}", reference);
+            // }
+
+            println!("refs: {:?}", references);
+
+            let table = Table::new(references).to_string();
+            println!("{}", table);
+        }
+
+        Err(ReadError(err)) | Err(ParseError(err)) => {
+            println!("Failed to parse csv file: {}", err);
+        }
+    }
 }
--- src/reference.rs
+++ src/reference.rs
@@ -1,6 +1,9 @@
+use core::fmt;
+// use std::{collections::HashMap, io::Error, io::ErrorKind, io::Read, path::Path};
+use std::{collections::HashMap, io::Read, path::Path};
 use tabled::Tabled;
 
-#[derive(Tabled)]
+#[derive(Tabled, Debug)]
 pub struct Reference {
     pub author: String,
     pub title: String,
@@ -12,6 +15,12 @@ pub struct Reference {
     pub month: String,
     pub note: String,
     pub key: String,
+}
+
+impl fmt::Display for Reference {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} ({})", self.author, self.year)
+    }
 }
 
 impl Reference {
@@ -30,9 +39,54 @@ impl Reference {
             key: "".to_string(),
         }
     }
+
+    pub fn set_field(&mut self, field: &Field, value: &String) -> &mut Reference {
+        match field {
+            Field::Author => {
+                self.author = value.to_string();
+                return self;
+            }
+            Field::Title => {
+                self.title = value.to_string();
+                return self;
+            }
+            Field::Year => {
+                self.year = value.to_string();
+                return self;
+            }
+            Field::Journal => {
+                self.journal = value.to_string();
+                return self;
+            }
+            Field::Volume => {
+                self.volume = value.to_string();
+                return self;
+            }
+            Field::Number => {
+                self.number = value.to_string();
+                return self;
+            }
+            Field::Pages => {
+                self.pages = value.to_string();
+                return self;
+            }
+            Field::Month => {
+                self.month = value.to_string();
+                return self;
+            }
+            Field::Note => {
+                self.note = value.to_string();
+                return self;
+            }
+            Field::Key => {
+                self.key = value.to_string();
+                return self;
+            }
+        }
+    }
 }
 
-pub fn example_references() -> [Reference; 2] {
+pub fn _example_references() -> [Reference; 2] {
     return [
         Reference {
             author: "Smith, John".to_string(),
@@ -59,4 +113,107 @@ pub fn example_references() -> [Reference; 2] {
             key: "doe2022".to_string(),
         },
     ];
+}
+
+pub enum CsvError {
+    ReadError(String),
+    ParseError(String),
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub enum Field {
+    Author,
+    Title,
+    Year,
+    Journal,
+    Volume,
+    Number,
+    Pages,
+    Month,
+    Note,
+    Key,
+}
+
+pub fn references_from_csv(path: &Path) -> Result<Vec<Reference>, CsvError> {
+    let mut file = std::fs::File::open(path).map_err(|err| CsvError::ReadError(err.to_string()))?;
+    let mut file_contents = String::new();
+
+    match file.read_to_string(&mut file_contents) {
+        Ok(_bytes) => {
+            let field_indices = get_field_indices(&file_contents)?;
+            // TODO
+            let references: Result<Vec<Reference>, CsvError> =
+                parse_references(&field_indices, &file_contents);
+            return references;
+        }
+        Err(err) => {
+            return Err(CsvError::ReadError(err.to_string()));
+        }
+    }
+}
+
+fn get_field_indices(csv: &String) -> Result<HashMap<Field, usize>, CsvError> {
+    let lines: Vec<&str> = csv.lines().collect();
+    // Map fields to their location in the csv file
+    let mut field_indices: HashMap<Field, usize> = HashMap::new();
+
+    if let Some(headers) = lines.get(0) {
+        let headers: Vec<&str> = headers.split(',').collect();
+
+        for (idx, header) in headers.iter().enumerate() {
+            match *header {
+                "author" => field_indices.insert(Field::Author, idx),
+                "title" => field_indices.insert(Field::Title, idx),
+                "year" => field_indices.insert(Field::Year, idx),
+                "journal" => field_indices.insert(Field::Journal, idx),
+                "volume" => field_indices.insert(Field::Volume, idx),
+                "number" => field_indices.insert(Field::Number, idx),
+                "pages" => field_indices.insert(Field::Pages, idx),
+                "month" => field_indices.insert(Field::Month, idx),
+                "note" => field_indices.insert(Field::Note, idx),
+                "key" => field_indices.insert(Field::Key, idx),
+                _ => None, // TODO return parse error here
+            };
+        }
+
+        Ok(field_indices)
+    } else {
+        return Err(CsvError::ParseError(
+            "Could not parse headers from CSV file: file contains no lines.".to_string(),
+        ));
+    }
+}
+
+fn parse_references(
+    field_indices: &HashMap<Field, usize>,
+    file_contents: &str,
+) -> Result<Vec<Reference>, CsvError> {
+    let rows: Vec<&str> = file_contents.lines().skip(1).collect();
+    for row in rows {
+        let cells: Vec<&str> = row.split(',').collect();
+        let mut currentReference = Reference::new_empty();
+
+        for (field, index) in field_indices.iter() {
+            println!("field: {:?} index: {index}", field);
+
+            let value = cells.get(*index);
+
+            currentReference.set_field(field, value);
+        }
+
+        // let authorIndex = field_indices.get(&Field::Author);
+        // let titleIndex = field_indices.get(&Field::Title);
+        // let yearIndex = field_indices.get(&Field::Year);
+        // let journalIndex = field_indices.get(&Field::Journal);
+        // let volumeIndex = field_indices.get(&Field::Volume);
+        // let numberIndex = field_indices.get(&Field::Number);
+        // let pagesIndex = field_indices.get(&Field::Pages);
+        // let monthIndex = field_indices.get(&Field::Month);
+        // let noteIndex = field_indices.get(&Field::Note);
+        // let keyIndex = field_indices.get(&Field::Key);
+    }
+
+    todo!();
+
+    // _returnval
 }
